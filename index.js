8e6efeb73204d37692ca1bdb239ca16060d41254
Programming Hero <programming.hero1@gmail.com>
2024-11-20 06:00:50 -0600

    created delete and get a coffee api

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 
@@ -36,6 +36,13 @@ async function run() {
             const cursor = coffeeCollection.find();
             const result = await cursor.toArray();
             res.send(result);
+        });
+
+        app.get('/coffee/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await coffeeCollection.findOne(query);
+            res.send(result);
         })
 
         app.post('/coffee', async (req, res) => {
@@ -43,6 +50,14 @@ async function run() {
             console.log('Adding new coffee', newCoffee)
 
             const result = await coffeeCollection.insertOne(newCoffee);
+            res.send(result);
+        });
+
+        app.delete('/coffee/:id', async (req, res) => {
+            console.log('going to delete', req.params.id);
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const result = await coffeeCollection.deleteOne(query);
             res.send(result);
         })
 
