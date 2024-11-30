2ec4c6229c830f9618a07100a2d8c881b91f29b1
ummesalma303 <ummesalma333999@gmail.com>
2024-11-30 16:57:17 +0600

    update api

--- index.js
+++ index.js
@@ -13,9 +13,6 @@ app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ot76b.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-// console.log(uri)
-// console.log(process.env.DB_PASS)
-// console.log(process.env.DB_USER)
 
 const client = new MongoClient(uri, {
     serverApi: {
